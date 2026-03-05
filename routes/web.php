2be138315113b705aff0e378658d02bc4e625430
badrus <m.badrussholeh@saintek.unipdu.ac.id>
2026-03-05 12:43:33 +0700

    feat: inisiasi project Laravel - M.FauzianAfshor

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/perkenalan', function () {
+
+    return '<h1>Halo! Nama saya M. Fauzian Afshor</h1>
+
+            <p>NIM: 4124033 | Prodi: Sistem Informasi</p>
+
+            <p>Saya siap belajar Laravel! 🚀</p>';
+
+});
